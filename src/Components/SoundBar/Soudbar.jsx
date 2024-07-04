0400d91cb1d98e0118f919d5b9edc071fee4031d
John Bale <103919889+IMTheBale@users.noreply.github.com>
2024-07-04 19:56:21 +0300

    updated SoundBar

--- src/Components/SoundBar/Soudbar.jsx
+++ src/Components/SoundBar/Soudbar.jsx
@@ -1,79 +1,87 @@
-import React, { useRef, useState } from 'react'
-import styled, { keyframes } from 'styled-components'
+import React, { useRef, useState, useEffect } from 'react';
+import styled, { keyframes } from 'styled-components';
 
-import music from "../SoundBar/Insurrection.mp3"
+import music from "../SoundBar/Insurrection.mp3";
 
 const Box = styled.div`
-display:flex;
-cursor:pointer;
-position:fixed;
-left:3.5rem;
-top:6rem;
-z-index:10;
-&>*:nth-child(1){
+  display: flex;
+  cursor: pointer;
+  position: fixed;
+  left: 3.5rem;
+  top: 6rem;
+  z-index: 10;
+
+  & > *:nth-child(1) {
     animation-delay: 0.2s;
-}
-&>*:nth-child(2){
+  }
+  & > *:nth-child(2) {
     animation-delay: 0.3s;
-}
-&>*:nth-child(3){
+  }
+  & > *:nth-child(3) {
     animation-delay: 0.4s;
-}
-&>*:nth-child(4){
+  }
+  & > *:nth-child(4) {
     animation-delay: 0.5s;
-}
-&>*:nth-child(5){
+  }
+  & > *:nth-child(5) {
     animation-delay: 0.8s;
-}
-`
+  }
+`;
 
 const play = keyframes`
-0%{
-    transform:scaleY(1);
-}
-50%{
-    transform:scaleY(2);
-}
-100%{
-    transform:scaleY(1);
-}
-`
+  0% {
+    transform: scaleY(1);
+  }
+  50% {
+    transform: scaleY(2);
+  }
+  100% {
+    transform: scaleY(1);
+  }
+`;
+
 const Line = styled.span`
-background: var(--blue-color);
-border: 1.6px solid;
-animation:${play} 1.4s ease infinite;
-animation-play-state: ${props => props.click ? "running" : "paused"};
-height: 1rem;
-width: 10px;
-margin:0 0.1rem
-`
+  background: var(--blue-color);
+  border: 1.6px solid;
+  animation: ${play} 1.4s ease infinite;
+  animation-play-state: ${props => (props.click ? 'running' : 'paused')};
+  height: 1rem;
+  width: 10px;
+  margin: 0 0.1rem;
+`;
 
 const SoundBar = () => {
+  const audioRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(true);
 
-    const ref = useRef(null);
-    const [click, setClick] = useState(false);
-
-    const handleClick = () => {
-        setClick(!click);
-
-        if(!click){
-            ref.current.play();
-        }else{
-            ref.current.pause();
-        }
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.volume = 0.1; // Set volume to 10%
+      audioRef.current.play(); // Start playing automatically
     }
-    return (
-        <Box onClick={() => handleClick()}>
-            <Line click={click}/>
-            <Line click={click}/>
-            <Line click={click}/>
-            <Line click={click}/>
-            <Line click={click}/>
+  }, []);
 
+  const handleToggle = () => {
+    setIsPlaying(!isPlaying);
 
-            <audio src={music} ref={ref} loop />
-        </Box>
-    )
-}
+    if (!isPlaying) {
+      audioRef.current.play();
+    } else {
+      audioRef.current.pause();
+    }
+  };
 
-export default SoundBar
+  return (
+    <Box onClick={handleToggle}>
+      <Line click={isPlaying} />
+      <Line click={isPlaying} />
+      <Line click={isPlaying} />
+      <Line click={isPlaying} />
+      <Line click={isPlaying} />
+
+      <audio src={music} ref={audioRef} loop autoPlay />
+    </Box>
+  );
+};
+
+export default SoundBar;

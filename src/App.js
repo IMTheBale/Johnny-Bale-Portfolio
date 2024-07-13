f38558bec521fc0c5ecf93e8b91f5986ae8be820
John Bale <103919889+IMTheBale@users.noreply.github.com>
2024-07-13 20:27:37 +0300

    updated portfolio

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Navbar from "./Components/Navbar";
 import Home from "./Sections/Home";
 import About from "./Sections/About";
-import Experience from "./Sections/Experience";
 import Work from "./Sections/Work";
 import Contact from "./Sections/Contact";
 import ContactBar from "./Components/ContactBar";
@@ -19,7 +18,6 @@ function App() {
 			<div className="app_section_container">
 				<Home />
 				<About />
-				<Experience />
 				<Work />
 				<Contact />
 			</div>

57dc112475b256081dbcf286b3b5b8041a9671ef
John Bale <103919889+IMTheBale@users.noreply.github.com>
2024-07-13 20:47:32 +0300

    imported facebook icon

--- src/Components/ContactBar/index.jsx
+++ src/Components/ContactBar/index.jsx
@@ -3,6 +3,7 @@ import TwitterIcon from '@mui/icons-material/Twitter';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import FacebookIcon from '@mui/icons-material/Facebook';
 import styles from "./styles.module.css";
 
 function ContactBar() {
